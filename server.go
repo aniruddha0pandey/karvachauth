a9ec8813139eec4e502c6fc31adb52760881d147
aniruddha0pandey <anirudh.pandev@gmail.com>
2019-10-25 15:00:00 +0530

    update

--- server.go
+++ server.go
@@ -6,14 +6,16 @@ import (
     "net/http"
 )
 
-var baseURL := "https://api.github.com"
-var code string
+var AccessCode string
+var AccessToken string
 
-var (
-	ClientID := "8d95898e238907f7f66f"
-	ClientSecret := "cb262234f230270d8235371749a16c9b582f54b5"
-	CallbackURL := "http://localhost:6969/oauth/callback"
-)
+// var (
+// 	ClientID := "8d95898e238907f7f66f"
+// 	ClientSecret := "cb262234f230270d8235371749a16c9b582f54b5"
+
+//     baseURL := "https://github.com"
+// 	CallbackURL := "http://localhost:6969/oauth/callback"
+// )
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "%s/\n", r.URL.Path[1:])
@@ -48,3 +50,11 @@ func main() {
     http.HandleFunc("/oauth/success", handleSuccess)
     log.Fatal(http.ListenAndServe(":6969", nil))
 }
+
+
+// GET baseURL/login/oauth/authorize?client_id=ClientID&redirect_uri=CallbackURL
+// GET CallbackURL?code=f867948d80c5ceee1b6c
+// POST baseURL/login/oauth/access_token?client_id=ClientID&client_secret=ClientSecret?code=AccessCode
+// GET access_token=0d490cd3e30f2be1e8c28529c9042f040059ff6a&scope=&token_type=bearer
+
+
